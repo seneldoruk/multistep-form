15926f438e1ce51600cfef4057bcd464afa09a91
Doruk Senel <seneldoruk@gmail.com>
2024-08-08 23:22:31 +0200

    form validation

--- src/form-steps/ThirdStep.tsx
+++ src/form-steps/ThirdStep.tsx
@@ -1,26 +1,50 @@
 import { Dispatch } from "react";
 import IconTextInput from "../components/IconTextInput";
-import { EmailSVG, EuroSVG, NameSVG, PhoneSVG } from "../components/Svgs";
+import {
+  EmailSVG,
+  EuroSVG,
+  NameSVG,
+  PhoneSVG,
+  WarningSVG,
+} from "../components/Svgs";
 import FormAnimationWrapper from "./FormAnimationWrapper";
+import { useFormContext } from "react-hook-form";
+import { FormInput } from "../schema";
 
 type Props = {
   setter: Dispatch<React.SetStateAction<boolean>>;
 };
 export default function FormThirdStep({ setter }: Props) {
+  const {
+    getValues,
+    formState: { isValid },
+  } = useFormContext<FormInput>();
+  const { fullName, email, phoneNumber, salary } = getValues();
+
   return (
     <FormAnimationWrapper>
-      <IconTextInput icon={<NameSVG />} value={"John Mustermann"} />
-      <IconTextInput icon={<EmailSVG />} value={"muster@muster.com"} />
-      <IconTextInput icon={<PhoneSVG />} value={"1511111111"} />
-      <IconTextInput icon={<EuroSVG />} value={"> 4000"} />
-      <button
-        className="btn btn-wide btn-primary"
-        onClick={() => {
-          setter(true);
-        }}
-      >
-        Submit!
-      </button>
+      <IconTextInput icon={<NameSVG />} value={fullName} />
+      <IconTextInput icon={<EmailSVG />} value={email} />
+      <IconTextInput icon={<PhoneSVG />} value={phoneNumber} />
+      <IconTextInput icon={<EuroSVG />} value={salary} />
+      {!isValid && (
+        <div role="alert" className="alert alert-warning">
+          <WarningSVG />
+          <span className="text-center">
+            You have to fill in the form correctly before submitting it
+          </span>
+        </div>
+      )}
+      {isValid && (
+        <button
+          className="btn btn-wide btn-primary"
+          onClick={() => {
+            setter(true);
+          }}
+        >
+          Submit!
+        </button>
+      )}
     </FormAnimationWrapper>
   );
 }
