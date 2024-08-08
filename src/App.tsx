60a2e5343de52f82c4722b1ef2d77440594324d2
Doruk Senel <seneldoruk@gmail.com>
2024-08-08 19:20:35 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen w-full flex items-center justify-center">
+        <div className="card bg-neutral text-neutral-content w-96">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Hi!</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
